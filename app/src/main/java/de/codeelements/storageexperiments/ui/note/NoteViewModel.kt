e0e4c334307a3141c9d44a9eac643e3ea50b891f
Maik Riechel <m.riechel@codeelements.de>
2018-11-16 11:55:00 +0100

    Fix detail loading stream

--- app/src/main/java/de/codeelements/storageexperiments/ui/note/NoteViewModel.kt
+++ app/src/main/java/de/codeelements/storageexperiments/ui/note/NoteViewModel.kt
@@ -3,6 +3,8 @@ package de.codeelements.storageexperiments.ui.note
 import android.arch.lifecycle.ViewModel
 import de.codeelements.storageexperiments.model.Note
 import de.codeelements.storageexperiments.storage.StorageProvider
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 
 class NoteViewModel : ViewModel() {
@@ -12,10 +14,20 @@ class NoteViewModel : ViewModel() {
 
     private val noteSubject = BehaviorSubject.createDefault<Note>(Note())
 
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+    }
+
     fun setCurrentNoteId(noteId: Long) {
         noteId.takeIf { it >= 0 }?.also {
-            storage.observeNote(it).take(1)
-                .subscribe(noteSubject)
+            compositeDisposable.add(
+                storage.observeNote(it).take(1)
+                    .subscribe { note ->
+                        noteSubject.onNext(note)
+                    }
+            )
         }
     }
 
@@ -25,5 +37,5 @@ class NoteViewModel : ViewModel() {
         }
     }
 
-    val noteObservable get() = noteSubject.replay(1).autoConnect()
+    val noteObservable get() = noteSubject as Observable<Note>
 }
